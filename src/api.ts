9d2b8aec611f579e0d9a861a524ac09eac0de2ea
Debbie Cheng <dqrcheng@gmail.com>
2023-03-06 23:21:16 +1100

    Remove foreignObject and move elements out of svg; Add lighting animations

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,5 @@
 const SCREEN_BOX_SHADOW = "0 0 5.5em 1.5em rgba(255, 255, 255, 0.66)";
-const SCREEN_GLOW_ANIMATION = "fuzzy-screen ease-in-out 0.5s infinite, glow ease-in 3s infinite";
+const SCREEN_GLOW_ANIMATION = "0.5s ease-in-out infinite fuzzy-screen, 3s ease-in infinite glow";
 
 type TrackData = {
   id: string;
@@ -50,16 +50,18 @@ export const setTrack = async () => {
   const svg = document.querySelectorAll<HTMLElement>(".svg-container");
   
   if (screen) {
-    screen.style.setProperty("--screenImage", `url("${result.tracks[1].cover}")`);
+    screen.style.setProperty("--screenImage", `url("${result.tracks[3].cover}")`);
     screen.style.setProperty("--screenGlow", SCREEN_BOX_SHADOW);
-    screen.style.animation += SCREEN_GLOW_ANIMATION;
+    screen.style.animation = SCREEN_GLOW_ANIMATION;
   }
   // if (progressBar) progressBar.style.display = "block";
   if (shadow) { 
     shadow.style.opacity = "0.5";
+    shadow.style.animation = "3s steps(2) infinite shadow-change";
   }
   if (svg && svg.length > 0) { 
     svg[0].style.filter = "brightness(100%)";
+    svg[0].style.animation = "3s steps(2) infinite light-change";
   }
   
 
@@ -67,7 +69,7 @@ export const setTrack = async () => {
   if (track) {
     track.pause();
   } else { 
-    track = new Audio(result.tracks[1].src);
+    track = new Audio(result.tracks[3].src);
     track.play();
   }
 };
